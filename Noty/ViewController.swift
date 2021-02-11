86e9a797cd51ae1121720b9db64e834c9911f08e
Youssef Jdidi <youssef.jdidi@esprit.tn>
2021-02-11 14:10:47 +0100

    Initial Commit

---
+++ Noty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noty
+//
+//  Created by Youssef Jdidi on 11/2/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
